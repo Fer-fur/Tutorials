f0559f98389f88f0050fbfee7c9b1692f6712336
Syfer <alvinwcj@gmail.com>
2024-05-24 19:04:17 +0800

    added responsiveness

--- form/ex1/script.js
+++ form/ex1/script.js
@@ -7,8 +7,12 @@ confirm_password.addEventListener('input', (e) => {
     if(input_password.value !== e.target.value) {
         passwordErrorMsg.style.display = 'block';
         passwordErrorPlaceholder.style.height = '0';
+        input_password.style.borderColor = 'red';
+        confirm_password.style.borderColor = 'red';
     }else {
         passwordErrorMsg.style.display = 'none';
         passwordErrorPlaceholder.style.height = '18px';
+        input_password.style.borderColor = 'blue';
+        confirm_password.style.borderColor = 'blue';
     }
 });
