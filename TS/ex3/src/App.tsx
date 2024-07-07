be9b3c1331dd9b91b3c27ac4af9990b1e885b6c1
Syfer <alvinwcj@gmail.com>
2024-07-07 20:44:19 +0800

    added state mgmt ex

---
+++ TS/ex3/src/App.tsx
@@ -0,0 +1,17 @@
+import { AppProvider } from "./AppContext";
+import { Header } from "./Header";
+import { Main } from "./Main";
+
+
+function App() {
+  return (
+    <div className="max-w-7xl mx-auto px-4">
+      <AppProvider>
+        <Header/>
+        <Main />
+      </AppProvider>
+    </div>
+  )
+}
+
+export default App;
